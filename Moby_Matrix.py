35e33bccf58022fb93fc6046d3bb27b5edf3299e
Jesse Bacon <jessembacon@me.com>
2015-06-12 22:57:31 -0500

    Added the Moby_Matrix.py script.  A utility for plotting letter frequency will be usefull in neuro/linguistic analysis.

---
+++ Moby_Matrix.py
@@ -0,0 +1,40 @@
+import matplotlib.pyplot as plt
+import nltk
+#Jesse Bacon
+#Putz Moby Dick into a Matrix and then graphs the charecter frequency distribution.
+#Ready for Styles and everything.
+#Requires NLTK and Matplotlib
+
+words = nltk.corpus.gutenberg.words('melville-moby_dick.txt')
+
+def letter_matrix():
+    cake = {}
+    alphabits = 'abcdefghijklmnopqrstuvwxyz'
+    cake = defaultdict(int)
+    for piece in alphabits:
+        cake[piece] += 0
+    return cake
+
+def zip_book(wordlist,matrix):
+    for word in wordlist:
+        for letter in word:
+            matrix[letter] += 1
+    return cake
+
+def make_frequency_chart(zipped_book, title):
+    lines = []
+    counts = []
+    for line, count in zipped_book.items():
+        lines.append(line)
+        counts.append(count)
+    plots = zip(lines,counts)
+    plt.xlabel('Character')
+    plt.ylabel('Count')
+    #plt.legend #ary
+    plt.plot(counts)
+    plt.xticks(range(0,len(lines)), lines, rotation=0)
+    plt.title(title)
+
+matrix = letter_matrix()
+frequency = zip_book(words, matrix)
+make_frequency_chart(frequency, 'Moby Dick')
