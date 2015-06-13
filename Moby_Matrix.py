315d179a7f1fd179a904c0826665a9362d9854df
Jesse Bacon <jessembacon@me.com>
2015-06-12 23:03:33 -0500

    Added the Moby_Matrix.py script.  A utility for plotting letter frequency will be usefull in neuro/linguistic analysis.

--- Moby_Matrix.py
+++ Moby_Matrix.py
@@ -22,7 +22,7 @@ def zip_book(wordlist,matrix):
             matrix[letter] += 1
     return matrix
 
-def make_frequency_chart(zipped_book, title):
+def make_frequency_chart(zipped_book, title, filename):
     lines = []
     counts = []
     for line, count in zipped_book.items():
@@ -35,7 +35,9 @@ def make_frequency_chart(zipped_book, title):
     plt.plot(counts)
     plt.xticks(range(0,len(lines)), lines, rotation=0)
     plt.title(title)
+    plt.savefig(filename)
+    plt.show()
 
 matrix = letter_matrix()
 frequency = zip_book(words, matrix)
-make_frequency_chart(frequency, 'Moby Dick')
+make_frequency_chart(frequency, 'Moby Dick', 'MobyDick.jpg')
