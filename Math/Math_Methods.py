dfce34c8532f8493cb48e37552438494acfd910a
Jesse Bacon <jessembacon@me.com>
2015-08-27 14:17:51 -0500

    Added methods for listing primes by sums of squares and a visualization.

--- Math/Math_Methods.py
+++ Math/Math_Methods.py
@@ -1,6 +1,9 @@
 #!Python
 #Jesse Bacon
 
+import matplotlib.pyplot as plt
+import numpy as np
+
 def determine_prime(number):
     '''
     determines prime numbers
@@ -79,3 +82,46 @@ def pascals_triangle_sums(triangle_object):
         bits.append(z)
         z = 0
     return bits
+
+def list_of_primes( start, end ):
+    assert type(start) == int, 'input a number for start'
+    assert type(end) == int, 'input a number for end'
+    return [ w for w in range( 3, end) \
+                     if determine_prime(w) == 'prime' and w >= start ]
+
+def list_of_primes_from_summed_squares( start, end ):
+    assert type(start) == int, 'input a number for start'
+    assert type(end) == int, 'input a number for end'
+    return [ w for w in range( 3, end) \
+                     if determine_prime(w) == 'prime' and w >= start \
+            and str(float(w) / 4.0)[-2:] == '25' ]
+
+def squares_for_primes( start, end ):
+    primes = list_of_primes_from_summed_squares( start, end )
+    primes_and_squares = {}
+    for i in primes:
+        for i2 in range(len(range(i))):
+            for i3 in range(len(range(i))):
+                if i2**2 + i3**2 == i:
+                    primes_and_squares[i] = (i2, i3)
+    return primes_and_squares
+
+def graph_primed_roots( start, end ):
+    primes = squares_for_primes( start, end )
+    N = len( primes )
+    ticks = ticks = tuple(sorted(primes))
+    square1 = [primes[i][0] for i in sorted(primes)]
+    square2 = [primes[i][1] for i in sorted(primes)]
+    ind = np.arange( N )
+    width = 0.36
+
+    p1 = plt.bar( ind, square1, width, color = 'r' )
+    p2 = plt.bar( ind, square2, width, color = 'b' )
+
+    plt.ylabel( 'prime' )
+    plt.title( 'Primes with Squared Sum roots' )
+    plt.xticks( ind + width/2., ticks )
+    plt.legend( (p1[0], p2[0]), ( 'Square1', 'Square2' ) )
+
+    plt.show()
+    return primes
